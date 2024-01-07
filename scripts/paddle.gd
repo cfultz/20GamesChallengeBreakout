3ec2ce6b765700821727da60f6d04996d9647b92
cfultz <caleb@fultz.dev>
2024-01-07 13:23:52 -0500

    Working paddle

--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -1,11 +1,12 @@
 extends RigidBody2D
 
+@export var speed = 500
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	var movement = Vector2.ZERO
+	if Input.is_action_pressed("left"):
+		movement = Vector2.LEFT
+	elif Input.is_action_pressed("right"):
+		movement = Vector2.RIGHT
+		
+	linear_velocity = movement * speed
